4b6502cb7aed9454be9b70d016ed29883b8a3fd5
mr excel <mrexcel153@gmail.com>
2018-12-18 11:50:31 +0000

    initial commit with the Todo schema created

---
+++ api/models/todo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const TodoSchema = new mongoose.Schema({
+  activity: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 5,
+  },
+  CreatedAt: {
+    type: Date,
+    default: Date.now()
+  },
+  isDone: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  isDoneDate: {
+    type: Date,
+    default: null,
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: false, //needs to be true but not now till every thing is settled.,
+    trim: true,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false
+  },
+});
+
+const Todos = mongoose.model('Todos', TodoSchema)
