f8c1c0910e3f9059b14fc88b08749b8cbe6414ab
mrexcel153 <mrexcel153@gmail.com>
2020-02-25 07:34:03 +0000

    adds a new manual cors method

--- app.js
+++ app.js
@@ -17,19 +17,38 @@ var app = express();
 //   origin: "*"
 // };
 
+const isPreflight = req => {
+  return (
+    req.method === "OPTIONS" &&
+    req.headers["origin"] &&
+    req.headers["access-control-request-method"]
+  );
+};
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  res.status(200);
+  res.set("Access-Control-Allow-Origin", "*");
+
+  if (isPreflight(req)) {
+    res.status(204).end();
+    return;
+  }
+
   next();
 });
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+//   );
+//   res.status(200);
+//   next();
+// });
 
 // allows api access from different origins]
 // app.use(cors(corsOption));
