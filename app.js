4b1895a56fd536c2894c94c3a2392c622f78ba70
mrexcel153 <mrexcel153@gmail.com>
2020-02-25 07:40:59 +0000

    adds different headers a new manual cors method

--- app.js
+++ app.js
@@ -27,6 +27,14 @@ const isPreflight = req => {
 
 app.use((req, res, next) => {
   res.set("Access-Control-Allow-Origin", "*");
+  res.set(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
 
   if (isPreflight(req)) {
     res.status(204).end();
